9788c2bd6499bbb02a99e5726838f02908e7156a
Marsel Zhusubaliev <marchello.marsel@gmail.com>
2019-12-23 09:24:21 +0300

    32

--- mine.js
+++ mine.js
@@ -1,8 +1,45 @@
-const menuJs = document.querySelectorAll(".menu-acco__name");
 
-menuJs.addEventListener('click', function (e) {
-  console.log(e);
+// menu acco
+
+$(function() {
+
+  var btn = $('.menu-acco__name');
+  var activeClass = 'menu-acco__item--active';
+
+  btn.click(function(event) {
+    event.preventDefault();
+    var parent = $(this).parent();
+
+      if (parent.hasClass(activeClass)) {
+        parent.removeClass(activeClass);
+      }
+
+      else {
+        btn.parent().removeClass(activeClass);
+        parent.addClass(activeClass);
+      }
+
+  });
 });
 
+// team acco
 
+$(function () {
 
+  var baton = $('.team-acco__name');
+  var activeClass = 'team-acco__container--active';
+
+  baton.click(function (event) {
+    event.preventDefault();
+    var parent = $(this).parent();
+
+    if (parent.hasClass(activeClass)) {
+      parent.removeClass(activeClass);
+    }
+    else {
+      baton.parent().removeClass(activeClass);
+      parent.addClass(activeClass);
+    }
+
+  });
+});
