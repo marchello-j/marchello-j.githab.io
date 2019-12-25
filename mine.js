f10d957e029223f716523554333f959d94c64322
Marsel Zhusubaliev <marchello.marsel@gmail.com>
2019-12-25 17:25:07 +0300

    slider

--- mine.js
+++ mine.js
@@ -233,3 +233,32 @@ function validateField(field) {
   }
 }
 
+/////// slider
+
+
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const list = document.querySelector("#list");
+
+const minRight = 0;
+const maxRight = 3840;
+const step = 960;
+let currentRight = 0;
+
+list.style.right = currentRight;
+
+right.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (currentRight < maxRight) {
+    currentRight += step;
+    list.style.right = currentRight + "px";
+  }
+});
+
+left.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (currentRight > minRight) {
+    currentRight -= step;
+    list.style.right = currentRight + "px";
+  }
+});
